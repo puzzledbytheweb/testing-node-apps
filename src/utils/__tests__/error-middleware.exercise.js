8f497bf55f7c7d50973fd6a721f184015d100b5a
puzzledbytheweb <pmsfilipe1@gmail.com>
2020-06-19 13:32:56 +0100

    fix(error-middleware): fix tests

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -1,16 +1,63 @@
 // Testing Middleware
 
 // 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
 
 // 🐨 you'll need both of these:
-// import {UnauthorizedError} from 'express-jwt'
-// import errorMiddleware from '../error-middleware'
+import {UnauthorizedError} from 'express-jwt'
+import errorMiddleware from '../error-middleware'
 
-// 🐨 Write a test for the UnauthorizedError case
-// 💰 const error = new UnauthorizedError('some_error_code', {message: 'Some message'})
-// 💰 const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
+const req = {}
+const next = jest.fn(() => next)
+const code = 'some_error_code'
+const message = 'Some message'
 
-// 🐨 Write a test for the headersSent case
+const error = new UnauthorizedError(code, {
+  message,
+})
 
-// 🐨 Write a test for the else case (responds with a 500)
+const getRes = (overrides) => {
+  const res = {
+    json: jest.fn(() => res),
+    status: jest.fn(() => res),
+    ...overrides,
+  }
+
+  return res
+}
+
+describe('errorMiddleware handles all cases', () => {
+  // 🐨 Write a test for the UnauthorizedError case
+  test('handles UnauthorizedError case, responding with a 401', () => {
+    const res = getRes()
+    errorMiddleware(error, req, res, next)
+
+    expect(next).not.toHaveBeenCalled()
+    expect(res.json).toHaveBeenCalledWith({
+      code: error.code,
+      message: error.message,
+    })
+    expect(res.json).toHaveBeenCalledTimes(1)
+    expect(res.status).toHaveBeenCalledWith(401)
+    expect(res.status).toHaveBeenCalledTimes(1)
+  })
+
+  // 🐨 Write a test for the headersSent case
+  test('handles headersSent error case, calling next', () => {
+    const res = getRes({headersSent: true})
+    errorMiddleware(error, req, res, next)
+
+    expect(next).toHaveBeenCalledWith(error)
+    expect(next).toHaveBeenCalledTimes(1)
+    expect(res.status).not.toHaveBeenCalled()
+    expect(res.json).not.toHaveBeenCalled()
+  })
+
+  // 🐨 Write a test for the else case (responds with a 500)
+  test('handles fallback case', () => {
+    const res = getRes()
+    const newError = new TypeError('Oh no type error')
+    errorMiddleware(newError, req, res, next)
+
+    expect(res.status).toBeCalledWith(500)
+  })
+})
